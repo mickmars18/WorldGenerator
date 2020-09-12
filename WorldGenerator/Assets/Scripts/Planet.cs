75274fd31c2a3b58fdc7c406941dd2a4f9549d92
alex3918 <alexandre.laboissonniere.1@ulaval.ca>
2020-09-12 13:55:12 -0400

    UVSphere

---
+++ WorldGenerator/Assets/Scripts/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(5, 256)]
+    public int precision = 40;
+
+    [Range(0.1f, 100.0f)]
+    public float m_radius = 1.0f;
+
+    public GameObject m_MeshGameObject;
+
+    private void OnValidate()
+    {
+        GeneratePlanet();
+    }
+
+    private void GeneratePlanet()
+    {
+        GenerateSphereMesh(precision, m_radius);
+    }
+
+    private void GenerateSphereMesh(int precision, float radius)
+    {
+        m_MeshGameObject.GetComponent<MeshFilter>().sharedMesh = UVSphereMeshGenerator.GenerateMesh(precision, radius);
+    }
+}
---
+++ WorldGenerator/Assets/Scripts/UVSphereMeshGenerator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class UVSphereMeshGenerator
+{
+    public static Mesh GenerateMesh(int precision, float radius)
+    {
+        Vector3[] vertices = GenerateVertices(precision, radius);
+        int[] triangles = GenerateTriangles(precision);
+        return GenerateMesh(vertices, triangles, precision);
+    }
+
+    private static Mesh GenerateMesh(Vector3[] vertices, int[] triangles, int precision)
+    {
+        Mesh mesh = new Mesh();
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+
+    private static Vector3[] GenerateVertices(int precision, float radius)
+    {
+        Vector3[] vertices = new Vector3[(precision + 1) * (precision + 1)];
+        for (int i = 0; i < precision + 1; i++)
+        {
+            float parallel = Mathf.PI * i / (float)precision;
+            for (int j = 0; j < precision + 1; j++)
+            {
+                float meridian = 2.0f * (float)Mathf.PI * j / (float)precision;
+                float xCoord = radius * Mathf.Sin(parallel) * Mathf.Cos(meridian);
+                float yCoord = radius * Mathf.Sin(parallel) * Mathf.Sin(meridian);
+                float zCoord = radius * Mathf.Cos(parallel);
+                vertices[i * (precision + 1) + j] = new Vector3(xCoord, yCoord, zCoord);
+            }
+        }
+        return vertices;
+    }
+
+    private static int[] GenerateTriangles(int precision)
+    {
+        int[] triangles = new int[precision * precision * 6];
+
+        int triangleIndex = 0;
+        for (int i = 0; i < precision; i++)
+        {
+            for (int j = 0; j < precision; j++)
+            {
+                triangles[triangleIndex] = j + i * (precision + 1);
+                triangles[triangleIndex + 1] = precision + j + 1 + i * (precision + 1);
+                triangles[triangleIndex + 2] = j + 1 + i * (precision + 1);
+
+                triangles[triangleIndex + 3] = precision + j + 1 + i * (precision + 1);
+                triangles[triangleIndex + 4] = precision + j + 2 + i * (precision + 1);
+                triangles[triangleIndex + 5] = j + 1 + i * (precision + 1);
+
+                triangleIndex += 6;
+            }
+        }
+        return triangles;
+    }
+}
